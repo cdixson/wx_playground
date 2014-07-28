b07382b2cdd3f0b85800a7e57517bc873b11e50c
Chris <cdixson@dixson.org>
2014-07-28 17:23:37 -0400

    added date time info

--- wx.py
+++ wx.py
@@ -1,17 +1,37 @@
-import json, requests
+import json, requests, datetime
+
+
+
+
 
 try:
 	r = requests.get("http://api.openweathermap.org/data/2.5/weather?lat=41.865&lon=-71.490")
+
 	data = r.json()
 
+	print (r.headers['content-type'])
 	tempKelvin = (data['main']['temp'])
 	tempCelcius = tempKelvin - 273.15
 	tempFarenheit = (tempCelcius * 1.8000) + 32.00
 	
+	#
 	theTime = (data['dt'])
-	print theTime
+	
+	print( datetime.datetime.fromtimestamp(int(theTime)).strftime('%Y-%m-%d %H:%M:%S %Z%z')
+	)
 
 	print tempFarenheit
 
 except requests.exceptions.RequestException as e:    
     print "cannot connect"
+
+
+#location
+#temp (increasing or decreasing in last hour, icon up/down)
+#barometric pressure
+#current conditions (icon)
+#alert
+
+
+
+
