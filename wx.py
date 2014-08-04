0815504e730b1188fcf98df7057fa8d7650a2d77
Chris <cdixson@dixson.org>
2014-08-04 15:31:50 -0400

    code clean up
    code clean up, threw out extraneous files

--- wx.py
+++ wx.py
@@ -1,37 +1,27 @@
 import json, requests, datetime
 
-
-
-
-
 try:
-	r = requests.get("http://api.openweathermap.org/data/2.5/weather?lat=41.865&lon=-71.490")
+	r = requests.get("http://api.openweathermap.org/data/2.5/weather?lat=41.865519&lon=-71.491376")
 
 	data = r.json()
 
 	print (r.headers['content-type'])
+
 	tempKelvin = (data['main']['temp'])
+
 	tempCelcius = tempKelvin - 273.15
 	tempFarenheit = (tempCelcius * 1.8000) + 32.00
 	
-	#
-	theTime = (data['dt'])
-	
-	print( datetime.datetime.fromtimestamp(int(theTime)).strftime('%Y-%m-%d %H:%M:%S %Z%z')
-	)
+	print(datetime.datetime.fromtimestamp(int(data['dt']))).strftime('%Y-%m-%d %H:%M:%S %Z%z')
 
+	print (data['name'])
 	print tempFarenheit
 
 except requests.exceptions.RequestException as e:    
     print "cannot connect"
-
 
 #location
 #temp (increasing or decreasing in last hour, icon up/down)
 #barometric pressure
 #current conditions (icon)
 #alert
-
-
-
-
